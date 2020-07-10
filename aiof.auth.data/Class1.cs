fcfcb2ef27caca2f5275d213608a7bf5121e3c15
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-09 20:51:31 -0400

    add initial projects

---
+++ aiof.auth.data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace aiof.auth.data
+{
+    public class Class1
+    {
+    }
+}
---
+++ aiof.auth.services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace aiof.auth.services
+{
+    public class Class1
+    {
+    }
+}
