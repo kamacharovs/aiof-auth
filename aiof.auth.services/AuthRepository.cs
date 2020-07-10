c503ad34d68be055ac3f8b3256fb88cc0dbe3317
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-10 12:26:18 -0400

    add packages and method to generate JWT

--- aiof.auth.services/AuthRepository.cs
+++ aiof.auth.services/AuthRepository.cs
@@ -1,6 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+using aiof.auth.data;
 
 namespace aiof.auth.services
 {
@@ -11,6 +19,26 @@ namespace aiof.auth.services
         public AuthRepository(ILogger<AuthRepository> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("secret");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] 
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
