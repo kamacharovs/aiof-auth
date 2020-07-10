c67fe0b2adc09187b0b9e4231a36fec83f41d47a
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-10 08:53:27 -0400

    add db context

---
+++ aiof.auth.data/AuthContext.cs
@@ -0,0 +1,32 @@
+using System;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace aiof.auth.data
+{
+    public class AuthContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        public AuthContext(DbContextOptions<AuthContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(e =>
+            {
+                e.ToTable("user");
+
+                e.HasKey(x => x.Id);
+
+                e.Property(x => x.FirstName).HasColumnName("first_name").HasMaxLength(200).IsRequired();
+                e.Property(x => x.LastName).HasColumnName("last_name").HasMaxLength(200).IsRequired();
+                e.Property(x => x.Email).HasColumnName("email").HasMaxLength(200).IsRequired();
+                e.Property(x => x.Username).HasColumnName("username").HasMaxLength(200).IsRequired();
+                e.Property(x => x.Password).HasColumnName("password").HasMaxLength(100).IsRequired();
+                e.Property(x => x.ApiKey).HasColumnName("api_key").HasMaxLength(1000);
+            });
+        }
+    }
+}
