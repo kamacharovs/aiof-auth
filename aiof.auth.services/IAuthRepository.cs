610e8d36c25a8079d9854c628aaa4887a935b6fc
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-17 11:27:35 -0400

    add client jwt generation

--- aiof.auth.services/IAuthRepository.cs
+++ aiof.auth.services/IAuthRepository.cs
@@ -9,6 +9,7 @@ namespace aiof.auth.services
     public interface IAuthRepository
     {
         ITokenResponse GenerateJwtToken(IUser user);
+        ITokenResponse GenerateJwtToken(IClient client);
         ITokenResponse GenerateJwtToken(IEnumerable<Claim> claims, IPublicKeyId entity = null);
         ITokenResult ValidateToken(string token);
         bool IsAuthenticated(string token);
