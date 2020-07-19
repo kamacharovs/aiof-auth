7a62fd69f54f681ac50451637992d2c4f87b809a
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-19 10:38:08 -0400

    add aiof claim validator

---
+++ aiof.auth.data/Validators/AiofClaimValidator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+using FluentValidation;
+
+namespace aiof.auth.data
+{
+    public class AiofClaimValidator : AbstractValidator<AiofClaim>
+    {
+        public AiofClaimValidator()
+        {
+            ValidatorOptions.Global.CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(x => x.Name)
+                .Must(x => AiofClaims.All.Contains(x))
+                .WithMessage("Invalid Claim. Valid Claims are: " + String.Join(",", AiofClaims.All));
+        }
+    }
+}
