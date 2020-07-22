ff95e9ae803d2b767eaf8d1854ed3e4b3ca8494e
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-22 14:57:53 -0400

    update validation unit tests

--- aiof.auth.tests/Validator.Tests.cs
+++ aiof.auth.tests/Validator.Tests.cs
@@ -12,10 +12,12 @@ namespace aiof.auth.tests
     public class ValidatorTests
     {
         private readonly AbstractValidator<UserDto> _userDtoValidator;
+        private readonly AbstractValidator<TokenRequest> _tokenRequestValidator;
 
         public ValidatorTests()
         {
             _userDtoValidator = Helper.GetRequiredService<AbstractValidator<UserDto>>() ?? throw new ArgumentNullException(nameof(AbstractValidator<UserDto>));
+            _tokenRequestValidator = Helper.GetRequiredService<AbstractValidator<TokenRequest>>()  ?? throw new ArgumentNullException(nameof(AbstractValidator<TokenRequest>));
         }
 
         [Theory]
@@ -40,6 +42,42 @@ namespace aiof.auth.tests
             userDto.Password = password;
 
             Assert.False(_userDtoValidator.Validate(userDto).IsValid);
+        }
+
+        [Theory]
+        [InlineData("test", "test", null)]
+        [InlineData(null, null, "test")]
+        public void TokenRequest_Valid(string username, string password, string apiKey)
+        {
+            var validation = _tokenRequestValidator
+                .Validate(new TokenRequest
+                {
+                    Username = username,
+                    Password = password,
+                    ApiKey = apiKey
+                });
+
+            Assert.True(validation.IsValid);
+        }
+
+        [Theory]
+        [InlineData("username", "pass", "apikey")]
+        [InlineData("username", null, null)]
+        [InlineData(null, "pass", null)]
+        [InlineData("username", null, "apikey")]
+        [InlineData(null, "pass", "apikey")]
+        [InlineData(null, null, null)]
+        public void TokenRequest_Invalid(string username, string password, string apiKey)
+        {
+            var validation = _tokenRequestValidator
+                .Validate(new TokenRequest
+                {
+                    Username = username,
+                    Password = password,
+                    ApiKey = apiKey
+                });
+
+            Assert.False(validation.IsValid);
         }
     }
 }
