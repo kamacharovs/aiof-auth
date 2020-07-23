e062116cc2264246b49441d5a678b30176c27f22
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-23 10:42:01 -0400

    update validator unit tests

--- aiof.auth.tests/Validator.Tests.cs
+++ aiof.auth.tests/Validator.Tests.cs
@@ -12,11 +12,13 @@ namespace aiof.auth.tests
     public class ValidatorTests
     {
         private readonly AbstractValidator<UserDto> _userDtoValidator;
+        private readonly AbstractValidator<ClientDto> _clientDtoValidator;
         private readonly AbstractValidator<TokenRequest> _tokenRequestValidator;
 
         public ValidatorTests()
         {
             _userDtoValidator = Helper.GetRequiredService<AbstractValidator<UserDto>>() ?? throw new ArgumentNullException(nameof(AbstractValidator<UserDto>));
+            _clientDtoValidator = Helper.GetRequiredService<AbstractValidator<ClientDto>>() ?? throw new ArgumentNullException(nameof(AbstractValidator<ClientDto>));
             _tokenRequestValidator = Helper.GetRequiredService<AbstractValidator<TokenRequest>>()  ?? throw new ArgumentNullException(nameof(AbstractValidator<TokenRequest>));
         }
 
@@ -42,6 +44,36 @@ namespace aiof.auth.tests
             userDto.Password = password;
 
             Assert.False(_userDtoValidator.Validate(userDto).IsValid);
+        }
+
+        [Theory]
+        [InlineData("test client", "test-client-slug")]
+        [InlineData("Client 1", "client-1-slug")]
+        public void ClientDto_Validate_Valid(string name, string slug)
+        {
+            var clientDto = new ClientDto
+            {
+                Name = name,
+                Slug = slug
+            };
+
+            Assert.True(_clientDtoValidator.Validate(clientDto).IsValid);
+        }
+
+        [Theory]
+        [InlineData("", "test-client-slug")]
+        [InlineData(null, "test-client-slug")]
+        [InlineData("Client 1", "")]
+        [InlineData("Client 1", null)]
+        public void ClientDto_Validate_Invalid(string name, string slug)
+        {
+            var clientDto = new ClientDto
+            {
+                Name = name,
+                Slug = slug
+            };
+
+            Assert.False(_clientDtoValidator.Validate(clientDto).IsValid);
         }
 
         [Theory]
