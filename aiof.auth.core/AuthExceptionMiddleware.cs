b8c861ebb805e685d37809bfa8c76368bf5d7507
Georgi Kamacharov <gkamacharov@gmail.com>
2020-07-27 09:55:50 -0400

    update libraries

--- aiof.auth.core/AuthExceptionMiddleware.cs
+++ aiof.auth.core/AuthExceptionMiddleware.cs
@@ -13,8 +13,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 
-using FluentValidation.Results;
-
 using aiof.auth.data;
 
 namespace aiof.auth.core
