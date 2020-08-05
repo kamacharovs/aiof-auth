d746ce5459209d8c12668103205699932c095477
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-05 16:19:59 -0400

    add auth problem detail

---
+++ aiof.auth.data/AuthProblemDetail.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace aiof.auth.data
+{
+    public class AuthProblemDetail
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
