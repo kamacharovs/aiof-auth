0f2b1ef3683acc815f6325caed64e415deb4ad0f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-11 07:18:42 -0400

    add open api to configuration

--- aiof.auth.data/Keys.cs
+++ aiof.auth.data/Keys.cs
@@ -24,6 +24,24 @@ namespace aiof.auth.data
         public const string SaltSize = nameof(SaltSize);
         public const string KeySize = nameof(KeySize);
 
+        public const string OpenApi = nameof(OpenApi);
+        public const string Version = nameof(Version);
+        public const string Title = nameof(Title);
+        public const string Description = nameof(Description);
+        public const string Contact = nameof(Contact);
+        public const string Name = nameof(Name);
+        public const string Email = nameof(Email);
+        public const string Url = nameof(Url);
+        public const string License = nameof(License);
+        public const string OpenApiVersion = nameof(OpenApi) + ":" + nameof(Version);
+        public const string OpenApiTitle = nameof(OpenApi) + ":" + nameof(Title);
+        public const string OpenApiDescription = nameof(Description) + ":" + nameof(Description);
+        public const string OpenApiContactName = nameof(OpenApi) + ":" + nameof(Contact) + ":" + nameof(Name);
+        public const string OpenApiContactEmail = nameof(OpenApi) + ":" + nameof(Contact) + ":" + nameof(Email);
+        public const string OpenApiContactUrl = nameof(OpenApi) + ":" + nameof(Contact) + ":" + nameof(Url);
+        public const string OpenApiLicenseName = nameof(OpenApi) + ":" + nameof(License) + ":" + nameof(Name);
+        public const string OpenApiLicenseUrl = nameof(OpenApi) + ":" + nameof(License) + ":" + nameof(Url);
+
         ///
         /// Caching keys
         ///
