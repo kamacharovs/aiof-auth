cf8ff8bc2b559ed1331f3d9b74310a867f36b028
Georgi Kamacharov <gkamacharov@gmail.com>
2020-08-20 09:55:16 -0400

    update user

--- aiof.auth.data/User.cs
+++ aiof.auth.data/User.cs
@@ -7,15 +7,47 @@ namespace aiof.auth.data
     public class User : IUser, 
         IPublicKeyId, IApiKey
     {
-        [JsonIgnore] public int Id { get; set; }
-        [JsonIgnore] public Guid PublicKey { get; set; } = Guid.NewGuid();
+        [JsonIgnore]
+        [Required]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        public Guid PublicKey { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(200)]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(200)]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string Username { get; set; }
-        [JsonIgnore] public string Password { get; set; }
-        [JsonIgnore] public string PrimaryApiKey { get; set; }
-        [JsonIgnore] public string SecondaryApiKey { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        [MaxLength(100)]
+        public string Password { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        [MaxLength(100)]
+        public string PrimaryApiKey { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        [MaxLength(100)]
+        public string SecondaryApiKey { get; set; }
+
+        [Required]
         public DateTime Created { get; set; } = DateTime.UtcNow;
     }
 
