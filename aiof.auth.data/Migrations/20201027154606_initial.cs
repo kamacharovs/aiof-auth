cc9f0f29a7cd27689dd30477ced9fa7093bf11ea
Georgi Kamacharov <gkamacharov@gmail.com>
2020-10-27 11:46:15 -0400

    add initial migration

---
+++ aiof.auth.data/Migrations/20201027154606_initial.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace aiof.auth.data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "claim",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(nullable: false),
+                    name = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_claim", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(nullable: false),
+                    name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "client",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(nullable: false),
+                    name = table.Column<string>(maxLength: 200, nullable: false),
+                    slug = table.Column<string>(maxLength: 50, nullable: false),
+                    enabled = table.Column<bool>(nullable: false),
+                    primary_api_key = table.Column<string>(maxLength: 100, nullable: false),
+                    secondary_api_key = table.Column<string>(maxLength: 100, nullable: false),
+                    role_id = table.Column<int>(nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_client", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_client_role_role_id",
+                        column: x => x.role_id,
+                        principalTable: "role",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(nullable: false),
+                    first_name = table.Column<string>(maxLength: 200, nullable: false),
+                    last_name = table.Column<string>(maxLength: 200, nullable: false),
+                    email = table.Column<string>(maxLength: 200, nullable: false),
+                    username = table.Column<string>(maxLength: 200, nullable: false),
+                    password = table.Column<string>(maxLength: 100, nullable: false),
+                    primary_api_key = table.Column<string>(maxLength: 100, nullable: true),
+                    secondary_api_key = table.Column<string>(maxLength: 100, nullable: true),
+                    role_id = table.Column<int>(nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_role_role_id",
+                        column: x => x.role_id,
+                        principalTable: "role",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "client_refresh_token",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(nullable: false),
+                    token = table.Column<string>(maxLength: 100, nullable: false),
+                    client_id = table.Column<int>(nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    expires = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    revoked = table.Column<DateTime>(type: "timestamp", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_client_refresh_token", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_client_refresh_token_client_client_id",
+                        column: x => x.client_id,
+                        principalTable: "client",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_refresh_token",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    public_key = table.Column<Guid>(nullable: false),
+                    token = table.Column<string>(maxLength: 100, nullable: false),
+                    user_id = table.Column<int>(nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    expires = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    revoked = table.Column<DateTime>(type: "timestamp", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_refresh_token", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_refresh_token_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_claim_name",
+                table: "claim",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_client_role_id",
+                table: "client",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_client_refresh_token_client_id",
+                table: "client_refresh_token",
+                column: "client_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_role_name",
+                table: "role",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_role_id",
+                table: "user",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_username",
+                table: "user",
+                column: "username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_refresh_token_user_id",
+                table: "user_refresh_token",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "claim");
+
+            migrationBuilder.DropTable(
+                name: "client_refresh_token");
+
+            migrationBuilder.DropTable(
+                name: "user_refresh_token");
+
+            migrationBuilder.DropTable(
+                name: "client");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "role");
+        }
+    }
+}
